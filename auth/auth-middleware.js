cc6659712fff91cde3283cbe4458288b685ef8d9
dominique maack <dominiquemaack@gmail.com>
2019-11-11 16:26:11 -0700

    added user router

---
+++ auth/auth-middleware.js
@@ -0,0 +1,24 @@
+const bcrypt = require('bcryptjs');
+
+const Users = require('../users/user-model');
+
+module.exports = (req, res, next) => {
+    let { username, password } = req.headers;
+
+    if(username && password) {
+        Users.findBy({ username })
+        .first()
+        .then(user => {
+            if(user && bcrypt.compareSync(password, user.password)) {
+                next();
+            } else {
+                res.status(401).json({ message: 'Invalid Credentials' })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'Server Error' })
+        })
+    } else {
+        res.status(400).json({ message: 'Please provide credentials' })
+    }
+}
